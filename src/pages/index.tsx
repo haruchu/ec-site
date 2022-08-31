971201c1f32c93a9b42588d50fee6187f8f65b17
haru <haruki6180@gmail.com>
2022-08-31 18:28:54 +0900

    index.tsxを少し修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import {db} from '../../firebase/firebase'
 import styles from '../styles/Home.module.css'
 
-const Home: NextPage = (data: any) => {
+const Home: NextPage = ({data}: any) => {
   return (
     <div className={styles.container}>
       <Head>
