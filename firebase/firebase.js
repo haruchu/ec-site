957aa73a80485ad6c32fe6f51b6c77283df194a6
haru <haruki6180@gmail.com>
2022-08-31 18:22:33 +0900

    firebase設定

---
+++ firebase/firebase.js
@@ -0,0 +1,12 @@
+var admin = require("firebase-admin");
+
+// ダウンロードしたJSONファイルをインポート。
+var serviceAccount = require("./serviceAccountKey.json");
+
+try {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
+} catch {}
+
+export const db = admin.firestore();
