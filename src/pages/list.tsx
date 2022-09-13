495760bbffff74a12258e45939d3b3ca92d28b29
haru <haruki6180@gmail.com>
2022-09-13 17:33:26 +0900

    無限スクロール実装

--- src/pages/list.tsx
+++ src/pages/list.tsx
@@ -1,36 +1,59 @@
 import type { GetServerSideProps, NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
+import { useState } from 'react';
 import { db } from '../../firebase/firebase';
 import { Item } from '../components/item';
 import styles from '../styles/Home.module.css';
-import { Wrapper } from '../styles/List';
+import { StyledInfiniteScroll } from '../styles/List';
 
 type ItemType = {
   name: string;
   price: number;
   imageId: string;
+  uploadDate: string;
 };
 
 type ListType = {
-  items: ItemType[];
+  defaultItems: ItemType[];
 };
 
-const List: NextPage = ({ items }: ListType) => {
+const PERITEM = 4;
+
+const List: NextPage = ({ defaultItems }: ListType) => {
+  const [items, setItems] = useState(defaultItems);
+  const [hasMore, setHasMore] = useState(true);
+
+  const loadMore = async (page: any) => {
+    db.collection('items')
+      .orderBy('uploadDate', 'desc')
+      .limit(1)
+      .startAfter(items[items.length - 1].uploadDate)
+      .onSnapshot(function (querySnapshot) {
+        const lists = [];
+        querySnapshot.forEach(function (doc) {
+          lists.push({ key: doc.id, ...doc.data() });
+        });
+        if (!lists.length) {
+          setHasMore(false);
+          return;
+        }
+        setTimeout(() => {
+          setItems([...items, ...lists]);
+        }, 10);
+      });
+  };
+
   return (
     <div className={styles.container}>
       <Head>
         <title>商品リスト</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-
-      <main className={styles.main}>
-        <Wrapper>
-          {items.map((item: ItemType, index: number) => (
-            <Item key={index} name={item.name} price={item.price} imageId={item.imageId} />
-          ))}
-        </Wrapper>
-      </main>
+      <StyledInfiniteScroll loadMore={loadMore} hasMore={hasMore}>
+        {items.map((item, index) => (
+          <Item key={index} name={item.name} price={item.price} imageId={item.imageId} />
+        ))}
+      </StyledInfiniteScroll>
     </div>
   );
 };
@@ -38,11 +61,11 @@ const List: NextPage = ({ items }: ListType) => {
 export default List;
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const colRef = db.collection('items');
+  const colRef = db.collection('items').orderBy('uploadDate', 'desc').limit(PERITEM);
   const cardSnap = await colRef.get();
-  const items = cardSnap.docs.map((doc) => ({
+  const defaultItems = cardSnap.docs.map((doc) => ({
     id: doc.id,
     ...doc.data(),
   }));
-  return { props: { items } };
+  return { props: { defaultItems } };
 };
--- src/styles/List.tsx
+++ src/styles/List.tsx
@@ -1,8 +1,12 @@
+import InfiniteScroll from 'react-infinite-scroller';
 import styled from 'styled-components';
 
-export const Wrapper = styled.div`
+export const StyledInfiniteScroll = styled(InfiniteScroll)`
+  min-height: 100vh;
   display: flex;
-  flex-wrap: wrap;
+  flex-direction: column;
   justify-content: center;
+  align-items: center;
   gap: 50px;
+  margin-bottom: 40px;
 `;
